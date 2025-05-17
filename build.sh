d2f054539f2cc9a4281c300336b17e2710ade314
saturneric <eric@bktus.com>
2025-05-17 15:20:50 +0200

    chore(build): switch from ninja to make build system

--- build.sh
+++ build.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 set -e
 
-MISSING_PKGS=()
-if ! command -v cmake &>/dev/null; then
-  MISSING_PKGS+=cmake
-fi
-if ! command -v ninja &>/dev/null; then
-  MISSING_PKGS+=ninja
-fi
-
-if [[ ${#MISSING_PKGS[@]} -gt 0 ]]; then
-  echo "Installing missing packages: ${MISSING_PKGS[*]}"
-  pacman -Sy --noconfirm "${MISSING_PKGS[@]}"
-fi
-
 SOURCE_DIR=$(pwd)
 BUILD_DIR="$SOURCE_DIR/build"
 
 mkdir -p "$BUILD_DIR"
 
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -S "$SOURCE_DIR" -B "$BUILD_DIR"
-cmake --build "$BUILD_DIR"
-
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -S "$SOURCE_DIR" -B "$BUILD_DIR"
+cmake --build "$BUILD_DIR" -- -j$(nproc)
